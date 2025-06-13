823658ca74dfc7d70e4b81d233dfbe13631c186e
sandesh-kumar25 <sandeshkumar8961@gmail.com>
2025-06-13 07:57:46 +0530

    Basic understanding

---
+++ Day_2/marksdistribution.py
@@ -0,0 +1,12 @@
+M=int(input('Enter the marks obtained:'))
+if M>75:
+    print('GRADE: O')
+elif 60<=M<=75:
+    print('GRADE:A')
+elif 50<=M<=60:
+    print("GRDAE B")
+elif 40<=M<=50:
+    print("GRADE C")
+elif M<40:
+    print("GRADE D")
+
