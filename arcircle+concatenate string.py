aa40b2d8f5af8181a53eded63a0838622c825e66
sandesh-kumar25 <sandeshkumar8961@gmail.com>
2025-06-13 07:44:18 +0530

    Add files via upload

---
+++ arcircle+concatenate string.py
@@ -0,0 +1,16 @@
+print("Please Enter the radius - ")
+r = int(input())
+area =3.14*r*r
+print("The area is - ")
+print(area)
+
+str1 = "SANDESH"
+str2 = " KUMAR "
+str33 = "GUPTA"
+print(str1+str2+str33)
+
+print("WHAT IS YOUR FATHER'S NAME - ")
+x = (input())
+print("YOUR'S FATHER'S NAME IS - " + x)
+
+
