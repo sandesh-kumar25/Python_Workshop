9df4a8a5b1c66dc3836f0f4ade1eb1029b092db1
sandesh-kumar25 <sandeshkumar8961@gmail.com>
2025-06-13 07:41:56 +0530

    Day_1
    Basic method of printing the results by using users choice.

---
+++ first.py
@@ -0,0 +1,22 @@
+""" for run use ctrl+B"""
+a=10    
+print(a)
+b=20.765789876
+print("The data type of a is",type(a))
+print("data type of b is",type(b))
+c=768
+d=1
+print("The data type of c",type(c))
+print(a)
+print(b)
+print(c)
+print("the sum is",a+b+c+d)
+
+"""input function"""
+x=int(input("enter no"))
+print(x)
+print("enter no")
+y=int(input())
+print(y)
+sum=x+y 
+print("the sum is",sum) 
